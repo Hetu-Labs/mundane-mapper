bc0b59ae889ae5cb10b47269449cfef03663039e
Arjun Verma <arjun.verma8412@gmail.com>
2023-09-08 00:19:05 +0530

    init

---
+++ lib/mundane_mapper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mundane_mapper/version"
+
+module MundaneMapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mundane_mapper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MundaneMapper do
+  it "has a version number" do
+    expect(MundaneMapper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
